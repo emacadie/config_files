f9862a458e0d0f99ff9a241069292f546edcc546
EMacAdie <EMacAdie@gmail.com>
2017-04-03 22:53:22 -0500

    Adding crontab info to script

--- scripts/setNameHosts.sh
+++ scripts/setNameHosts.sh
@@ -1,2 +1,5 @@
+#- every 5 minutes in crontab
+# */5 * * * * root /root/scripts/setNameHosts.sh
+
 echo 'nameserver 8.8.8.8' | cat >> /etc/resolv.conf
 echo 'nameserver 8.8.4.4' | cat >> /etc/resolv.conf
