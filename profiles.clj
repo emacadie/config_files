8498353d9bf28da65ce7796afcbc58d0fd7f4fbe
Eric MacAdie <eric@MacAdie.net>
2017-07-03 01:04:32 -0500

    Added Chicked Scheme init file, dir for emacs from Brave Clojure

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
 ;[lein-droid "0.3.0"] @clojars.org
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   [lein-droid "0.3.0"] ]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
