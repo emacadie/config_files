91db989ef4f36465db793cf430bd22f1008c5d03
Eric MacAdie <eric@MacAdie.net>
2018-03-13 06:31:06 -0500

    Added copy command in comments to postgres conf files

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,8 @@
 ;; /home/$USER/.lein/profiles.clj
 ;[lein-droid "0.3.0"] @clojars.org
-{:user {:plugins [[cider/cider-nrepl "0.15.1"]
+{:user {:plugins [ ;[cider/cider-nrepl "0.15.1"]
+                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]
+                  [lein-autoreload "0.1.1"]
+                  [jonase/eastwood "0.2.5"]
                   [lein-droid "0.3.0"] ]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
