4bef5f570d34df90b6db0f47ba5fdeae3463052b
Eric MacAdie <eric@MacAdie.net>
2018-08-27 00:33:49 -0500

    Updating lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,14 +1,16 @@
 ;; /home/$USER/.lein/profiles.clj
 ;[lein-droid "0.3.0"] @clojars.org
 {:user {:plugins [
-                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]
-                  [jonase/eastwood "0.2.5"]
+                  [cider/cider-nrepl "0.18.0"]
+                  ; [cider/cider-nrepl "0.16.0-SNAPSHOT"]
+                  ; [jonase/eastwood "0.2.5"]
+                  [jonase/eastwood "0.2.9" :exclusions [[org.clojure/clojure]]]
                   [lein-droid "0.3.0"] 
                   ; [lein-autoreload "0.1.1"]
                   ]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/tools.namespace "0.2.11"]
                        ]
-        ; :repl-options { :init-ns user }
+        :repl-options { :init-ns user }
         :resource-paths ["~/.lein/src"]}}
 
