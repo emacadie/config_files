b076f517155ed93b0ad914cf60d6399a9d6dfa2a
Eric MacAdie <eric@MacAdie.net>
2018-09-06 20:16:03 -0500

    Some lein changes and JDK 11 bashrc

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,16 +1,17 @@
 ;; /home/$USER/.lein/profiles.clj
 ;[lein-droid "0.3.0"] @clojars.org
 {:user {:plugins [
-                  [cider/cider-nrepl "0.18.0"]
-                  ; [cider/cider-nrepl "0.16.0-SNAPSHOT"]
-                  ; [jonase/eastwood "0.2.5"]
+                  ; [cider/cider-nrepl "0.18.0"]
+                  [cider/cider-nrepl "0.19.0-SNAPSHOT"]
+                  
                   [jonase/eastwood "0.2.9" :exclusions [[org.clojure/clojure]]]
                   [lein-droid "0.3.0"] 
                   ; [lein-autoreload "0.1.1"]
                   ]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [org.clojure/tools.namespace "0.2.11"]
                        ]
-        :repl-options { :init-ns user }
-        :resource-paths ["~/.lein/src"]}}
+        ; :repl-options { :init-ns user }
+        :resource-paths ["~/.lein/src"]
+        }}
 
