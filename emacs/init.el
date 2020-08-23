b589e2928104b5e29f2071468f6f1f09ef83f293
Eric MacAdie <eric@MacAdie.net>
2020-08-23 16:40:35 -0500

    Made a change to emacs init.el

--- emacs/init.el
+++ emacs/init.el
@@ -166,6 +166,9 @@
                           116 120))
 (setq-default tab-width 4)
 (setq-default c-basic-offset 4)
+; from https://www.emacswiki.org/emacs/IndentationBasics
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
 ; (setq indent-line-function 'insert-tab)
 
 ;; (setq tab-width 4) ; or any other preferred value
@@ -212,9 +215,36 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default)))
  '(package-selected-packages
    (quote
-    (smooth-scrolling smartparens faceup s racket-mode dirtrack-buffer-name-track-mode gradle-mode json-mode tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous groovy-mode exec-path-from-shell clojure-mode-extra-font-locking cider))))
+    (go-mode 
+     kotlin-mode 
+     smooth-scrolling 
+     smartparens 
+     faceup 
+     s 
+     racket-mode 
+     dirtrack-buffer-name-track-mode 
+     gradle-mode 
+     json-mode 
+     tagedit 
+     smex 
+     rainbow-delimiters 
+     projectile 
+     paredit 
+     magit 
+     ido-ubiquitous 
+     groovy-mode 
+     exec-path-from-shell 
+     clojure-mode-extra-font-locking 
+     cider)))
+ '(safe-local-variable-values
+   (quote
+    ((cider-ns-refresh-after-fn . "integrant.repl/resume")
+     (cider-ns-refresh-before-fn . "integrant.repl/suspend")))))
 ;; commenting out stuff I might not use
 ;; geiser
 (custom-set-faces
--- my.init.el
+++ my.init.el
@@ -19,7 +19,7 @@
 ;; (global-font-lock-mode 1)
 
 ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
-;; (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+; (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 ; (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 ; (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 
